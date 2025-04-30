2a1ce34174d9d2bf1d5aaa1ee9bef0af1e7c05d0
rubel04 <alomrana886@gmail.com>
2025-04-30 11:48:50 +0600

    add logout system and dynamically show dashboard page, only logged in user can see dashboard route

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -5,28 +5,60 @@ import {
   NavbarLink,
   NavbarToggle,
 } from "flowbite-react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
 const NavBar = () => {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const { user, logoutUser } = useAuth();
+  const handleLogout = () => {
+    logoutUser()
+      .then(() => {
+        Swal.fire({
+          icon: "success",
+          title: "You logout successfully.",
+          timer: 1500,
+        });
+        navigate("/");
+      })
+      .catch(() => {
+        Swal.fire({
+          icon: "error",
+          title: "Something went wrong!",
+          timer: 1500,
+        });
+      });
+  };
+
   return (
     <div className="w-7xl mx-auto ">
-      <Navbar className={pathname !== '/' ? "bg-[#F1494C] mt-4":  'bg-none'} fluid rounded>
-      <NavbarBrand>
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          MarryMatch
-        </span>
-      </NavbarBrand>
-      <NavbarToggle />
-      <NavbarCollapse>
-        <NavbarLink href="/" active>
-          Home
-        </NavbarLink>
-        <NavbarLink href="#">About</NavbarLink>
-        <NavbarLink href="#">Services</NavbarLink>
-        <NavbarLink href="#">Contact</NavbarLink>
-        <NavbarLink href="/login">Login</NavbarLink>
-      </NavbarCollapse>
-    </Navbar>
+      <Navbar
+        className={pathname !== "/" ? "bg-[#F1494C] mt-4" : "bg-none"}
+        fluid
+        rounded
+      >
+        <NavbarBrand>
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            MarryMatch
+          </span>
+        </NavbarBrand>
+        <NavbarToggle />
+        <NavbarCollapse>
+          <NavbarLink href="/" active>
+            Home
+          </NavbarLink>
+          {user && <NavbarLink href="/dashboard">Dashboard</NavbarLink>}
+          <NavbarLink href="/biodatas">Biodatas</NavbarLink>
+          <NavbarLink href="/about">About Us</NavbarLink>
+          <NavbarLink href="/contact">Contact Us</NavbarLink>
+          {user ? (
+            <button onClick={handleLogout}>Log Out</button>
+          ) : (
+            <NavbarLink href="/login">Login</NavbarLink>
+          )}
+        </NavbarCollapse>
+      </Navbar>
     </div>
   );
 };
